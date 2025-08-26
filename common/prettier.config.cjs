fd7d03cee3767c8c00b1653aeb0395014cb45ad0
James Hu <james.hu.ustc@hotmail.com>
2025-08-26 23:03:45 +1000

    fix: remove copied files from git

--- common/prettier.config.cjs
+++
@@ -1,6 +0,0 @@
-const { prettierConfig } = require('eslint-config-sensible-prettier-typescript');
-
-module.exports = {
-  ...prettierConfig(),
-  // Override or extend the default config
-};
--- jest/eslint.config.cjs
+++
@@ -1,7 +0,0 @@
-const { eslintConfig } = require('eslint-config-sensible-prettier-typescript');
-const { defineConfig } = require('eslint/config');
-
-module.exports = defineConfig([
-  ...eslintConfig(),
-  // Add your customizations here
-]);
