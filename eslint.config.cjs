df3f469b1112543fa39d73227fe7aa4219fd1a51
James Hu <james.hu.ustc@hotmail.com>
2025-08-26 23:56:34 +1000

    feat: ignore fixture directories correctly

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,6 +2,10 @@ const { eslintConfig } = require('eslint-config-sensible-prettier-typescript');
 const { defineConfig } = require('eslint/config');
 
 module.exports = defineConfig([
+  // These global ignore patterns are just examples, feel free to remove or modify
+  {
+    ignores: ['mocha/test/fixtures/**/*', 'jest/test/fixtures/**/*'],
+  },
   ...eslintConfig(),
   // Add your customizations here
 ]);
